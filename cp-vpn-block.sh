08de272a2f665e4664798ce97c702252ad38bdce
mmuyskens <mark@loop9.com>
2019-08-10 13:06:33 -0700

    Update cp-vpn-block.sh
    Adding lastrun state to prevent IPs that were manually allowed from being blocked again.

--- cp-vpn-block.sh
+++ cp-vpn-block.sh
@@ -8,18 +8,26 @@ wget $IP_LIST
 echo "Cleaning up list..."
 sed -e 's/#.*$//' -e '/^$/d' $FILE_NAME > tmp.txt
 
+echo "Comparing to last run state..."
+
+if [ -f "lastrun.txt" ]; then
+    fgrep -vf lastrun.txt tmp.txt > dif.txt
+else
+    echo "No last run state detected..."
+    cp tmp.txt dif.txt
+fi
+
 echo "Starting list parsing..."
-cat tmp.txt | while read LINE
+cat dif.txt | while read LINE
 do
-    echo 'cpaneld : '$LINE' : deny' >> hosts.allow
+    echo 'cpaneld : '$LINE' : deny' >> /etc/hosts.allow
 done
 
-echo "Overwriting hosts.allow..."
-cp -R hosts.allow /etc/hosts.allow
+echo "Saving lastrun state..."
+mv -f tmp.txt lastrun.txt
 
 echo "Cleaning up..."
+rm dif.txt
 rm $FILE_NAME
-rm tmp.txt
-rm hosts.allow
 
 echo "Done!"
